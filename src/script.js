b24317a12f386811bf9632629e482864093e488a
Valērijs Kostjukevičs <166384039+23DP4VKost@users.noreply.github.com>
2025-10-20 21:54:54 +0300

    upd script.js

--- src/script.js
+++ src/script.js
@@ -67,7 +67,28 @@ document.getElementById('contactForm').addEventListener('submit', function(event
     const message = document.getElementById('message').value.trim();
     
 })
+
+
 /// search bar
 
+document.addEventListener('DOMContentLoaded', () => {
+    const searchInput = document.getElementById('search-input');
+    const cards = document.querySelectorAll('.card');
+
+    searchInput.addEventListener('input', () => {
+        const searchText = searchInput.value.toLowerCase();
+
+        cards.forEach(card => {
+            const cardText = card.querySelector('h3').textContent.toLowerCase() + 
+                             card.querySelector('p').textContent.toLowerCase();
+
+            if (cardText.includes(searchText)) {
+                card.style.display = 'block';
+            } else {
+                card.style.display = 'none';
+            }
+        });
+    });
+});
 
 });

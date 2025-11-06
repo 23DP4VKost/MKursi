63cc12d465ecbce35fe9f8b42b28a010ce27f32c
Valerijs Kostjukevics <A230168VK@rvt.lv>
2025-11-06 10:31:01 +0200

    Pivienot notes html un css daļa.Mainīts saturs.

---
+++ src/script.js
@@ -0,0 +1,74 @@
+// Melna tema
+document.addEventListener('DOMContentLoaded', function() {
+    const themeToggle = document.getElementById('theme-toggle');
+    
+    // saglab tema
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'dark') {
+        document.body.classList.add('dark-mode');
+        themeToggle.textContent = 'Mainīt temu uz balto';
+    }
+    
+    // Mainit tekst ar temu
+    themeToggle.addEventListener('click', function() {
+        document.body.classList.toggle('dark-mode');
+        
+        if (document.body.classList.contains('dark-mode')) {
+            localStorage.setItem('theme', 'dark');
+            themeToggle.textContent = 'Mainīt temu uz balto';
+        } else {
+            localStorage.setItem('theme', 'light');
+            themeToggle.textContent = 'Mainīt temu uz melno';
+        }
+    });
+
+/// modal 
+const modal = document.getElementById('course-modal');
+const cardBtn = document.querySelector('.card-btn');
+const closeModal = document.querySelector('.close-modal');
+
+cardBtn.addEventListener('click', function() {
+    modal.style.display = 'block';
+});
+
+closeModal.addEventListener('click', function() {
+    modal.style.display = 'none';
+});
+
+window.addEventListener('click', function(event) {
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+});
+
+
+
+const modal2 = document.getElementById('course-modal2');
+const cardBtn2 = document.querySelector('.card-btn2');
+const closeModal2 = document.querySelector('.close-modal2');
+
+cardBtn2.addEventListener('click', function() {
+    modal2.style.display = 'block';
+});
+
+closeModal2.addEventListener('click', function() {
+    modal2.style.display = 'none';
+});
+
+window.addEventListener('click', function(event) {
+    if (event.target === modal) {
+        modal2.style.display = 'none';
+    }
+});
+
+/// hamburger
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener('click', function() {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+});
+
+
+});

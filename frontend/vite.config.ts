03a8a4afad133d431a9fcd607a8edf5ed49a751b
VK <A230168VK@rvt.lv>
2026-05-16 14:11:21 +0300

    error 404 vercel

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,20 +1,16 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import path from 'path';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: path.resolve(__dirname, '../frontend/public/build'), 
+    outDir: 'dist',
     emptyOutDir: true,
-    manifest: true,
-    rollupOptions: {
-      input: path.resolve(__dirname, 'src/main.ts'), 
-    },
   },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
   },
-});
+})
